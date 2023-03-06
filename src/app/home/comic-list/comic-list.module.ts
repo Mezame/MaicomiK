729d101292968ddb85258d645429093271b1d301
Mezame <mezamesoft@gmail.com>
2023-03-06 12:33:06 -0600

    feat: Add comic-list page structure

---
+++ src/app/home/comic-list/comic-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComicListComponent } from './comic-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    ComicListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComicListModule { }
