78e63a5fd2504e2d40fd786c4a2260a67e0b2f7f
Mezame <mezamesoft@gmail.com>
2023-03-06 10:56:10 -0600

    feat(routes): Add basic routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'login',
+  loadChildren: () =>
+    import('./login/login.module').then((m) => m.LoginModule)
+},
+{
+  path: 'home',
+  loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+},
+{
+  path: '',
+  redirectTo: '',
+  pathMatch: 'full',
+},
+{ path: '**', redirectTo: 'login' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
