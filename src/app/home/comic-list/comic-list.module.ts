fc26971949b2fcd449ef134826a691c9e3812efb
Mezame <mezamesoft@gmail.com>
2023-03-06 14:29:33 -0600

    fix: Add missing routes configuration

--- src/app/home/comic-list/comic-list.module.ts
+++ src/app/home/comic-list/comic-list.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { ComicListComponent } from './comic-list.component';
 
-
+const routes: Routes = [{ path: '', component: ComicListComponent }];
 
 @NgModule({
-  declarations: [
-    ComicListComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ComicListComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class ComicListModule { }
+export class ComicListModule {}
