eb45b41045bde1dfc3764116f77602c38400d0cd
Mezame <mezamesoft@gmail.com>
2023-03-06 09:54:12 -0600

    feat(core): Add logger service

---
+++ src/app/core/logger/logger.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggerService {
+
+  logs: string[] = [];
+  devMode: boolean = true;
+  debugMode: boolean = false;
+
+  log: (message: string) => void = this.info;
+
+  constructor() {}
+
+  info(message: string) {
+    if (!this.devMode) return;
+
+    console.log(message);
+  }
+
+  error(message: string) {
+    if (!this.devMode) return;
+
+    if (this.debugMode) {
+      console.error(message);
+    }
+  }
+}
