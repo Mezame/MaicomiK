c1518d443498458dd1069bf201c15692eae8c013
Mezame <mezamesoft@gmail.com>
2023-03-06 14:16:36 -0600

    feat: Add comic-list page route

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'comic-list',
+        loadChildren: () =>
+          import('./comic-list/comic-list.module').then(
+            (m) => m.ComicListModule
+          ),
+      },
+      {
+        path: '',
+        redirectTo: 'comic-list',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
