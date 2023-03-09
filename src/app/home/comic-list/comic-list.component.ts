5faa88be6057d9cfc98130ce0acccd4f4a1dfeda
Mezame <mezamesoft@gmail.com>
2023-03-08 22:21:26 -0600

    fix: Add onPush change detection

--- src/app/home/comic-list/comic-list.component.ts
+++ src/app/home/comic-list/comic-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Comic } from '@features/comics/comic';
+import { comicsMock } from 'src/testing/comics.mock';
 
 @Component({
   selector: 'app-comic-list',
   templateUrl: './comic-list.component.html',
-  styleUrls: ['./comic-list.component.scss']
+  styleUrls: ['./comic-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ComicListComponent {
-  
+  comics!: Comic[];
+
+  constructor() {
+    this.comics = comicsMock;
+  }
 }
