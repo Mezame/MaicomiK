cec4f407bd7a7a2a8381b93826ba5bdc77d7ab10
Mezame <mezamesoft@gmail.com>
2023-03-08 22:21:03 -0600

    feat: Add primary-layout and comic-list-items modules

--- src/app/home/comic-list/comic-list.module.ts
+++ src/app/home/comic-list/comic-list.module.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PrimaryLayoutModule } from 'src/app/shared/layouts/primary-layout/primary-layout.module';
+
+import { ComicListItemsModule } from '@features/comics/comic-list-items/comic-list-items.module';
+import { PrimaryLayoutModule } from '@shared/layouts/primary-layout/primary-layout.module';
 import { ComicListComponent } from './comic-list.component';
 
 const routes: Routes = [{ path: '', component: ComicListComponent }];
 
 @NgModule({
   declarations: [ComicListComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), PrimaryLayoutModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    PrimaryLayoutModule,
+    ComicListItemsModule,
+  ],
 })
 export class ComicListModule {}
