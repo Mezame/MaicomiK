b9752c8a840eb6d75eecc5758d2798c8d1148383
Mezame <mezamesoft@gmail.com>
2023-03-10 14:32:45 -0600

    feat: Implement tag common-ui component

--- src/app/features/comics/comic-list-items/comic-list-items.module.ts
+++ src/app/features/comics/comic-list-items/comic-list-items.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { TagModule } from '@shared/common-ui/tag/tag.module';
 import { ComicListItemsComponent } from './comic-list-items.component';
 
 @NgModule({
   declarations: [ComicListItemsComponent],
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, TagModule, MatButtonModule, MatIconModule],
   exports: [ComicListItemsComponent],
 })
 export class ComicListItemsModule {}
