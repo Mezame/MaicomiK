a908912417a5d89da6685019ea0fc583709a224b
Mezame <mezamesoft@gmail.com>
2023-03-10 22:50:50 -0600

    feat: Add toolbar interactivity

--- src/app/shared/layouts/primary-layout/primary-layout.component.ts
+++ src/app/shared/layouts/primary-layout/primary-layout.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-primary-layout',
@@ -7,6 +14,23 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PrimaryLayoutComponent {
+  @ViewChild('toolbar') toolbar!: MatToolbar;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    const scrollTop = event.target.documentElement.scrollTop;
+
+    if (scrollTop > 22) {
+      this.renderer.addClass(this.toolbar._elementRef.nativeElement, 'sticky');
+    } else {
+      this.renderer.removeClass(
+        this.toolbar._elementRef.nativeElement,
+        'sticky'
+      );
+    }
+  }
+
+  constructor(private renderer: Renderer2) {}
 
   logOut() {}
 }
