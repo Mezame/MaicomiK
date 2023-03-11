590177185ebac56fe7fb9164ca747fdc05cbe726
Mezame <mezamesoft@gmail.com>
2023-03-11 12:21:50 -0600

    testing: Add missing imports and providers

--- src/app/home/comic-list/comic-list.component.spec.ts
+++ src/app/home/comic-list/comic-list.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { ComicListItemsModule } from '@features/comics/comic-list-items/comic-list-items.module';
+import { PrimaryLayoutModule } from '@shared/layouts/primary-layout/primary-layout.module';
 
 import { ComicListComponent } from './comic-list.component';
 
@@ -7,10 +11,17 @@ describe('ComicListComponent', () => {
   let fixture: ComponentFixture<ComicListComponent>;
 
   beforeEach(async () => {
+    const routeSpy = jasmine.createSpyObj('ActivatedRoute', ['']);
+
     await TestBed.configureTestingModule({
-      declarations: [ ComicListComponent ]
-    })
-    .compileComponents();
+      imports: [
+        NoopAnimationsModule,
+        PrimaryLayoutModule,
+        ComicListItemsModule,
+      ],
+      declarations: [ComicListComponent],
+      providers: [{ provide: ActivatedRoute, useValue: routeSpy }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ComicListComponent);
     component = fixture.componentInstance;
--- src/app/shared/layouts/primary-layout/primary-layout.component.spec.ts
+++ src/app/shared/layouts/primary-layout/primary-layout.component.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
 
 import { PrimaryLayoutComponent } from './primary-layout.component';
+import { PrimaryLayoutModule } from './primary-layout.module';
 
 describe('PrimaryLayoutComponent', () => {
   let component: PrimaryLayoutComponent;
   let fixture: ComponentFixture<PrimaryLayoutComponent>;
 
   beforeEach(async () => {
+    const routeSpy = jasmine.createSpyObj('ActivatedRoute', ['']);
+
     await TestBed.configureTestingModule({
-      declarations: [ PrimaryLayoutComponent ]
-    })
-    .compileComponents();
+      imports: [NoopAnimationsModule, PrimaryLayoutModule],
+      declarations: [PrimaryLayoutComponent],
+      providers: [{ provide: ActivatedRoute, useValue: routeSpy }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PrimaryLayoutComponent);
     component = fixture.componentInstance;
