039f2ad735284d385be14e30228a0ac2b103a19a
Mezame <mezamesoft@gmail.com>
2023-03-12 14:20:02 -0600

    fix: Add OnPush change detection strategy

--- src/app/shared/common-ui/tag/tag.component.ts
+++ src/app/shared/common-ui/tag/tag.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tag',
   templateUrl: './tag.component.html',
   styleUrls: ['./tag.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TagComponent {}
