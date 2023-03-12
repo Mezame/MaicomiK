2363b948cdfb9d0f3219b0d29f1459cba93f6f00
Mezame <mezamesoft@gmail.com>
2023-03-12 14:21:00 -0600

    testing: Update test

--- src/app/home/comic-list/comic-list.component.spec.ts
+++ src/app/home/comic-list/comic-list.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute } from '@angular/router';
 import { ComicListItemsModule } from '@features/comics/comic-list-items/comic-list-items.module';
+import { ComicsService } from '@features/comics/comics.service';
 import { PrimaryLayoutModule } from '@shared/layouts/primary-layout/primary-layout.module';
 
 import { ComicListComponent } from './comic-list.component';
@@ -9,9 +10,13 @@ import { ComicListComponent } from './comic-list.component';
 describe('ComicListComponent', () => {
   let component: ComicListComponent;
   let fixture: ComponentFixture<ComicListComponent>;
+  let comicsService: jasmine.SpyObj<ComicsService>;
 
   beforeEach(async () => {
     const routeSpy = jasmine.createSpyObj('ActivatedRoute', ['']);
+    const comicsServiceSpy = jasmine.createSpyObj('comicsService', [
+      'getComics',
+    ]);
 
     await TestBed.configureTestingModule({
       imports: [
@@ -20,8 +25,15 @@ describe('ComicListComponent', () => {
         ComicListItemsModule,
       ],
       declarations: [ComicListComponent],
-      providers: [{ provide: ActivatedRoute, useValue: routeSpy }],
+      providers: [
+        { provide: ActivatedRoute, useValue: routeSpy },
+        { provide: ComicsService, useValue: comicsServiceSpy },
+      ],
     }).compileComponents();
+
+    comicsService = TestBed.inject(
+      ComicsService
+    ) as jasmine.SpyObj<ComicsService>;
 
     fixture = TestBed.createComponent(ComicListComponent);
     component = fixture.componentInstance;
