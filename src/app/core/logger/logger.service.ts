ca730cd2c13d6b02ce58ceeac2b3cd588d898ce5
Mezame <mezamesoft@gmail.com>
2023-03-12 14:28:37 -0600

    fix: Add isDevMode from environment

--- src/app/core/logger/logger.service.ts
+++ src/app/core/logger/logger.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoggerService {
   logs: string[] = [];
-  devMode: boolean = true;
+  isDevMode: boolean = environment.isDevMode;
 
   log: (message: string) => void = this.info;
 
   constructor() {}
 
   info(message: string) {
-    if (!this.devMode) return;
+    if (!this.isDevMode) return;
 
     console.log(message);
   }
 
   error(message: string) {
-    if (!this.devMode) return;
+    if (!this.isDevMode) return;
 
     console.error(message);
   }
