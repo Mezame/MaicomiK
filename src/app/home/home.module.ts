ad21a756679ebd207bc10a3fbcce4e873aa01f8d
Mezame <mezamesoft@gmail.com>
2023-03-14 13:58:25 -0600

    feat: Add store and effects modules

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
+import { ComicEffects } from '@features/comics/state/comics.effects';
+import { comicsReducer } from '@features/comics/state/comics.reducer';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature('comics', comicsReducer),
+    EffectsModule.forFeature([ComicEffects]),
+    HomeRoutingModule,
+  ],
 })
 export class HomeModule {}
