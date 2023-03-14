6984beb093d8078e7296191d91b46062442813f2
Mezame <mezamesoft@gmail.com>
2023-03-14 13:50:37 -0600

    feat: Add load-comics action

--- src/app/features/comics/state/comics.actions.ts
+++ src/app/features/comics/state/comics.actions.ts
@@ -1,9 +1,20 @@
-import { createActionGroup, props } from '@ngrx/store';
+import { createAction, createActionGroup, props } from '@ngrx/store';
 import { Comic } from '../comic';
+
+export const LoadComicsAction = createAction('[Comic List Page] Load Comics');
+
+export const ComicsActions = createActionGroup({
+  source: 'Comics',
+  events: {
+    'Add Comics': props<{ comic: Comic }>(),
+  },
+});
 
 export const ComicsApiActions = createActionGroup({
   source: 'Comics API',
   events: {
     'Retrieved Comic List': props<{ comics: readonly Comic[] }>(),
+
+    'Added Comic': props<{ comic: Comic }>(),
   },
 });
