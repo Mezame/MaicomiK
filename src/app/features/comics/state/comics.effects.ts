295e3e23ca39a5918d877a38d88c443f94fce39f
Mezame <mezamesoft@gmail.com>
2023-03-14 13:52:04 -0600

    fix: Change load-comics behaviour

--- src/app/features/comics/state/comics.effects.ts
+++ src/app/features/comics/state/comics.effects.ts
@@ -1,25 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, EMPTY, exhaustMap, map } from 'rxjs';
+import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
+import { select, Store } from '@ngrx/store';
+import { EMPTY, map, mergeMap } from 'rxjs';
 import { ComicsService } from '../comics.service';
-import { ComicsApiActions } from './comics.actions';
+import { ComicsApiActions, LoadComicsAction } from './comics.actions';
+import { selectComics } from './comics.selectors';
 
 @Injectable()
 export class ComicEffects {
   loadComics$ = createEffect(() =>
     this.actions$.pipe(
-      ofType('[Comic List Page] Load Comics'),
-      exhaustMap(() =>
-        this.comicsService.getComics().pipe(
-          map((comics) => ComicsApiActions.retrievedComicList({ comics })),
-          catchError(() => EMPTY)
-        )
-      )
+      ofType(LoadComicsAction),
+      concatLatestFrom(() => this.store.pipe(select(selectComics))),
+      mergeMap(([_, comics]) => {
+        if (comics?.length > 0) {
+          return EMPTY;
+        }
+
+        return this.comicsService
+          .getComics()
+          .pipe(
+            map((comics) => ComicsApiActions.retrievedComicList({ comics }))
+          );
+      })
     )
   );
 
   constructor(
     private actions$: Actions,
-    private comicsService: ComicsService
+    private comicsService: ComicsService,
+    private store: Store
   ) {}
 }
