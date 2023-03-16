41068b0a866b004d7553ea64dbdaea4638be8f80
Mezame <mezamesoft@gmail.com>
2023-03-15 19:36:32 -0600

    fix: Remove bottom-sheet component

--- src/app/shared/ui-components/bottom-sheet/bottom-sheet.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-bottom-sheet',
-  templateUrl: './bottom-sheet.component.html',
-  styleUrls: ['./bottom-sheet.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class BottomSheetComponent {}
