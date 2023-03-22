61b29243c1b9cdbe78ab59d459e7737ec5517270
Mezame <mezamesoft@gmail.com>
2023-03-22 00:01:07 -0600

    feat: Add comic-detail-bottom-sheet module

--- src/app/home/comic-detail-page/comic-detail-page.module.ts
+++ src/app/home/comic-detail-page/comic-detail-page.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ComicDetailContentModule } from '@features/comics/comic-detail-content/comic-detail-content.module';
 import { PrimaryLayoutModule } from '@shared/layouts/primary-layout/primary-layout.module';
+import { ComicDetailBottomSheetModule } from '../comic-detail-bottom-sheet/comic-detail-bottom-sheet.module';
 import { ComicDetailPageComponent } from './comic-detail-page.component';
 
 const routes: Routes = [{ path: '', component: ComicDetailPageComponent }];
@@ -15,6 +16,7 @@ const routes: Routes = [{ path: '', component: ComicDetailPageComponent }];
     RouterModule.forChild(routes),
     PrimaryLayoutModule,
     ComicDetailContentModule,
+    ComicDetailBottomSheetModule
   ],
 })
 export class ComicDetailPageModule {}
