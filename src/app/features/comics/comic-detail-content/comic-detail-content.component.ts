b6383999358faafe79876e2cddbf69aecb6a8977
Mezame <mezamesoft@gmail.com>
2023-03-22 00:00:17 -0600

    feat: Add comic-detail-content component

--- src/app/features/comics/comic-detail-content/comic-detail-content.component.ts
+++ src/app/features/comics/comic-detail-content/comic-detail-content.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Comic } from '../comic';
 
 @Component({
   selector: 'app-comic-detail-content',
   templateUrl: './comic-detail-content.component.html',
-  styleUrls: ['./comic-detail-content.component.scss']
+  styleUrls: ['./comic-detail-content.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ComicDetailContentComponent {
+  @Input() comic!: Comic;
 
+  @Output() actionEvent = new EventEmitter<{
+    action: string;
+    data: Comic;
+  }>();
+
+  constructor() {}
+
+  emitOpenBottomSheetAction(data: Comic) {
+    const action = 'OpenBottomSheet';
+
+    this.actionEvent.emit({ action, data });
+  }
 }
--- src/app/features/comics/comic-detail-content/comic-detail-content.module.ts
+++ src/app/features/comics/comic-detail-content/comic-detail-content.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+
 import { ComicDetailContentComponent } from './comic-detail-content.component';
-
-
+import { TagModule } from '@shared/ui-components/tag/tag.module';
 
 @NgModule({
-  declarations: [
-    ComicDetailContentComponent
-  ],
+  declarations: [ComicDetailContentComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TagModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatListModule,
   ],
-  exports: [
-    ComicDetailContentComponent
-  ],
+  exports: [ComicDetailContentComponent],
 })
-export class ComicDetailContentModule { }
+export class ComicDetailContentModule {}
