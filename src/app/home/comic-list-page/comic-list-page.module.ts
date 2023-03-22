6292c4e7099cfc19e8189691d726d851eca5eeca
Mezame <mezamesoft@gmail.com>
2023-03-21 23:58:39 -0600

    feat: Add mat-button and mat-icon modules

--- src/app/home/comic-list-page/comic-list-page.module.ts
+++ src/app/home/comic-list-page/comic-list-page.module.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ComicListItemsModule } from '@features/comics/comic-list-items/comic-list-items.module';
 import { PrimaryLayoutModule } from '@shared/layouts/primary-layout/primary-layout.module';
 import { ComicListPageComponent } from './comic-list-page.component';
 
-const routes: Routes = [{ path: '', component: ComicListPageComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ComicListPageComponent,
+  },
+];
 
 @NgModule({
   declarations: [ComicListPageComponent],
@@ -15,6 +22,8 @@ const routes: Routes = [{ path: '', component: ComicListPageComponent }];
     RouterModule.forChild(routes),
     PrimaryLayoutModule,
     ComicListItemsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
 })
 export class ComicListPageModule {}
