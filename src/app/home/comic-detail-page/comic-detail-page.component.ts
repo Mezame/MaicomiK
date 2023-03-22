b2ec939c2b21f8f8c8f37f1ff91661832cb5033c
Mezame <mezamesoft@gmail.com>
2023-03-22 00:04:14 -0600

    feat: Add component view logic

--- src/app/home/comic-detail-page/comic-detail-page.component.ts
+++ src/app/home/comic-detail-page/comic-detail-page.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { ActivatedRoute } from '@angular/router';
 import { Comic } from '@features/comics/comic';
+import { ComicDetailBottomSheetComponent } from 'src/app/home/comic-detail-bottom-sheet/comic-detail-bottom-sheet.component';
+import { LoadComicsAction } from '@features/comics/state/comics.actions';
 import { selectComic } from '@features/comics/state/comics.selectors';
 import { Store } from '@ngrx/store';
 import { map, Observable } from 'rxjs';
@@ -14,9 +17,31 @@ import { map, Observable } from 'rxjs';
 export class ComicDetailPageComponent {
   comic$: Observable<Comic>;
 
-  constructor(private route: ActivatedRoute, private store: Store) {
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store,
+    private _bottomSheet: MatBottomSheet
+  ) {
     const comicUrlSegment = this.route.snapshot?.params['comicUrlSegment'];
 
-    this.comic$ = this.store.select(selectComic(comicUrlSegment));
+    this.comic$ = this.store.select(selectComic(comicUrlSegment)).pipe(
+      map((comic) => {
+        if (!comic) {
+          this.store.dispatch(LoadComicsAction());
+
+          return {} as Comic;
+        }
+
+        return { ...comic };
+      })
+    );
+  }
+
+  openBottomSheet(event: { action: string; data: Comic }): void {
+    const comic = event.data;
+
+    this._bottomSheet.open(ComicDetailBottomSheetComponent, {
+      data: { comic },
+    });
   }
 }
