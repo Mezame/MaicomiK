5c9b988b45abb91efa61b5ad739192998734175e
Mezame <mezamesoft@gmail.com>
2023-03-23 14:20:19 -0600

    fix: Add routing config

--- src/app/home/comic-add-edit-page/comic-add-edit-page.module.ts
+++ src/app/home/comic-add-edit-page/comic-add-edit-page.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PrimaryLayoutModule } from '@shared/layouts/primary-layout/primary-layout.module';
 import { ComicAddEditPageComponent } from './comic-add-edit-page.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: ComicAddEditPageComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    ComicAddEditPageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ComicAddEditPageComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), PrimaryLayoutModule],
 })
-export class ComicAddEditPageModule { }
+export class ComicAddEditPageModule {}
