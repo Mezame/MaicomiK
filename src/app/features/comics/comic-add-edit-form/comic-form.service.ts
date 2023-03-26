0535cde688e4f40e58e326520e46e5cbe24e3a0b
Mezame <mezamesoft@gmail.com>
2023-03-25 23:40:12 -0600

    feat: Add comic-form service

---
+++ src/app/features/comics/comic-add-edit-form/comic-form.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Comic } from '../comic';
+import { ComicFormValue } from './comic-form-value';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ComicFormService {
+  constructor() {}
+
+  formatChapter(comicFormValue: Readonly<ComicFormValue>) {
+    let chapter: number;
+    let formatedComic: Comic;
+
+    chapter = parseInt(comicFormValue.chapter!);
+    formatedComic = { ...comicFormValue } as unknown as Comic;
+    formatedComic.chapter = chapter;
+
+    return formatedComic;
+  }
+}
