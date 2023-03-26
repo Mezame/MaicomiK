68b3a9c09e4a8d6110cfa0adf051399270cfd71d
Mezame <mezamesoft@gmail.com>
2023-03-25 23:39:46 -0600

    fix: Add extra functionality to setDocumentNoId method

--- src/app/shared/firebase/firestore.service.ts
+++ src/app/shared/firebase/firestore.service.ts
@@ -12,6 +12,7 @@ import {
   Firestore,
   getDoc,
   setDoc,
+  Timestamp,
   updateDoc,
 } from '@angular/fire/firestore';
 import { LoggerService } from '@core/logger/logger.service';
@@ -116,19 +117,30 @@ export class FirestoreService {
   @param document An Object containing the data for the new document.
   @returns A promise with a Firestore custom response.
   **/
-  async setDocumentNoId(path: string, document: any) {
+  async setDocumentNoId(path: string, document: Readonly<any>) {
     let collRef: CollectionReference<DocumentData>;
     let docRef: DocumentReference<DocumentData>;
     let docRefId: string;
+    let currentTimestamp: Timestamp;
+    let newDocument: any;
     let res: any;
 
     try {
       collRef = collection(this.firestore, path);
       docRef = doc(collRef);
       docRefId = docRef?.id;
-      document.metadata.id = docRefId;
+      currentTimestamp = Timestamp.fromDate(new Date());
+      newDocument = {
+        ...document,
+        metadata: {
+          ...document['metadata'],
+          id: docRefId,
+          createdAt: currentTimestamp,
+          updatedAt: currentTimestamp,
+        },
+      };
 
-      res = await setDoc(docRef, document);
+      res = await setDoc(docRef, newDocument);
 
       if (res !== undefined) {
         throw new Error('could not add document');
@@ -138,7 +150,11 @@ export class FirestoreService {
         `FirestoreGlobalService: setDocumentNoId: added document w/ id=${docRefId}`
       );
 
-      return { success: true, id: docRefId } as FirestoreResponse;
+      return {
+        success: true,
+        document: newDocument,
+        id: docRefId,
+      } as FirestoreResponse;
     } catch (error) {
       return { error } as FirestoreResponse;
     }
