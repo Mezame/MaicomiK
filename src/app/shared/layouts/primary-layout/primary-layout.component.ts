402b0ff0911dc089bb2c5fc539a000b189217e06
Mezame <mezamesoft@gmail.com>
2023-03-27 15:41:04 -0600

    feat: Add sticky-footer functionality

--- src/app/shared/layouts/primary-layout/primary-layout.component.ts
+++ src/app/shared/layouts/primary-layout/primary-layout.component.ts
@@ -1,7 +1,10 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   HostListener,
+  OnInit,
   Renderer2,
   ViewChild,
 } from '@angular/core';
@@ -13,24 +16,45 @@ import { MatToolbar } from '@angular/material/toolbar';
   styleUrls: ['./primary-layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PrimaryLayoutComponent {
+export class PrimaryLayoutComponent implements OnInit, AfterViewInit {
   @ViewChild('toolbar') toolbar!: MatToolbar;
+  @ViewChild('body') body!: ElementRef<HTMLDivElement>;
+  @ViewChild('footer') footer!: ElementRef<HTMLDivElement>;
 
   @HostListener('window:scroll', ['$event'])
   onScroll(event: any) {
     const scrollTop = event.target.documentElement.scrollTop;
+    const toolbarEl = this.toolbar._elementRef.nativeElement;
 
     if (scrollTop > 22) {
-      this.renderer.addClass(this.toolbar._elementRef.nativeElement, 'sticky');
+      this.renderer.addClass(toolbarEl, 'sticky-toolbar');
     } else {
-      this.renderer.removeClass(
-        this.toolbar._elementRef.nativeElement,
-        'sticky'
-      );
+      this.renderer.removeClass(toolbarEl, 'sticky-toolbar');
     }
   }
 
   constructor(private renderer: Renderer2) {}
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    const deviceHeight = window.innerHeight;
+    const maxBodyContent = deviceHeight - 80 - 56;
+    const bodyEl = this.body.nativeElement;
+    const footerEl = this.footer.nativeElement;
+    const observer = new ResizeObserver((entries) => {
+      const height = entries[0].contentRect.height;
+      console.log(height);
+      console.log(maxBodyContent);
+
+      if (height > maxBodyContent) {
+        this.renderer.addClass(footerEl, 'sticky-footer');
+      } else {
+        this.renderer.removeClass(footerEl, 'sticky-footer');
+      }
+    });
+
+    observer.observe(bodyEl);
+  }
 
   logOut() {}
 }
