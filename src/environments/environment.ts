92f8a5e5c06293b4adeb73fa006f3b209ff0dd9d
Mezame <mezamesoft@gmail.com>
2023-03-29 14:42:55 -0600

    fix: Remove firebase data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,5 @@
 export const environment = {
   isDevMode: false,
   useEmulators: false,
-  firebase: {
-    projectId: 'maicomik',
-    appId: '1:1097288835978:web:37e302d1381e1bacf85e73',
-    storageBucket: 'maicomik.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyBOw7KmFtxPs7RbvS9Y_J_PrC0B9v6Va6Q',
-    authDomain: 'maicomik.firebaseapp.com',
-    messagingSenderId: '1097288835978',
-  },
+  firebase: {},
 };
