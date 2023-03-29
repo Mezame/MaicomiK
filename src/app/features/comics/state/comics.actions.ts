5584d5d6e33a5b639c5ffe46c2a0d0875d734bd7
Mezame <mezamesoft@gmail.com>
2023-03-29 16:48:10 -0600

    feat: Add increment-comic-chapter action

--- src/app/features/comics/state/comics.actions.ts
+++ src/app/features/comics/state/comics.actions.ts
@@ -3,9 +3,20 @@ import { Comic } from '../comic';
 
 export const LoadComicsAction = createAction('[Comic List Page] Load Comics');
 
-export const ComicAddAction = createAction('[Comic Add Page] Add Comic', props<{ comic: Partial<Comic> }>());
+export const ComicAddAction = createAction(
+  '[Comic Add Page] Add Comic',
+  props<{ comic: Partial<Comic> }>()
+);
 
-export const ComicEditAction = createAction('[Comic Edit Page] Edit Comic', props<{ comic: Readonly<Comic> }>());
+export const ComicEditAction = createAction(
+  '[Comic Edit Page] Edit Comic',
+  props<{ comic: Readonly<Comic> }>()
+);
+
+export const incrementComicChapterAction = createAction(
+  '[Comic Detail Page] Increment Comic Chapter',
+  props<{ comic: Readonly<Comic>, fields: Partial<Comic> }>()
+);
 
 export const ComicsApiActions = createActionGroup({
   source: 'Comics API',
