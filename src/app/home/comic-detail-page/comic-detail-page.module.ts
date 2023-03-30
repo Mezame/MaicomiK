71a675058a73714905b6542c5d53feb92b564c32
Mezame <mezamesoft@gmail.com>
2023-03-30 16:09:06 -0600

    fix: Add missing imports

--- src/app/home/comic-detail-page/comic-detail-page.module.ts
+++ src/app/home/comic-detail-page/comic-detail-page.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatListModule } from '@angular/material/list';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ComicDetailContentModule } from '@features/comics/comic-detail-content/comic-detail-content.module';
 import { PrimaryLayoutModule } from '@shared/layouts/primary-layout/primary-layout.module';
-import { ComicDetailBottomSheetModule } from '../comic-detail-bottom-sheet/comic-detail-bottom-sheet.module';
 import { ComicDetailPageComponent } from './comic-detail-page.component';
 
 const routes: Routes = [{ path: '', component: ComicDetailPageComponent }];
@@ -16,7 +17,8 @@ const routes: Routes = [{ path: '', component: ComicDetailPageComponent }];
     RouterModule.forChild(routes),
     PrimaryLayoutModule,
     ComicDetailContentModule,
-    ComicDetailBottomSheetModule
+    MatBottomSheetModule,
+    MatListModule,
   ],
 })
 export class ComicDetailPageModule {}
