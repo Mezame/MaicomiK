c4585ecbe3331b40d7e2d44e47b614b58e5fe842
Mezame <mezamesoft@gmail.com>
2023-04-01 12:47:49 -0600

    feat: Add icon

---
+++ src/app/shared/ui-components/icon/icon.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+import { IconComponent } from './icon.component';
+
+@NgModule({
+  declarations: [IconComponent],
+  imports: [MatIconModule],
+  exports: [IconComponent],
+})
+export class IconModule {}
