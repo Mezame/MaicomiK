3da4d17b6c0c5a60d3e7dc520b91a3c3330715f1
Mezame <mezamesoft@gmail.com>
2023-04-03 11:50:09 -0600

    feat: Add toolbar component

--- src/app/shared/ui-components/toolbar/toolbar.component.ts
+++ src/app/shared/ui-components/toolbar/toolbar.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToolbarComponent {
 
