7abac12023dc2bc6e85b1f1e71aac6b39ed795cc
Mezame <mezamesoft@gmail.com>
2023-04-03 11:51:23 -0600

    feat: Add home component functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,56 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { FooterPortalService } from '@shared/layouts/primary-layout/footer-portal/footer-portal.service';
+import { Observable, filter } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  template: '<router-outlet></router-outlet>',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
+  currentUrl: string;
+  footer$!: Observable<TemplateRef<any> | null>;
 
+  @ViewChild('comicListFooter') comicListFooter!: TemplateRef<any>;
+
+  constructor(
+    private router: Router,
+    private footerPortalService: FooterPortalService
+  ) {
+    this.currentUrl = '';
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        if (!!event.url) {
+          this.currentUrl = event.url;
+        }
+      });
+  }
+
+  ngOnInit(): void {
+    this.footer$ = this.footerPortalService.getFooter();
+  }
+
+  ngAfterViewInit(): void {}
+
+  ngOnDestroy(): void {}
+
+  setFooter() {
+    switch (this.currentUrl) {
+      case '/home/comics':
+        break;
+      default:
+    }
+  }
 }
