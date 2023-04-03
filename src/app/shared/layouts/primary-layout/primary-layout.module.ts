fd53fb5182c2fe6f7d8c92cfda632e87a660a32f
Mezame <mezamesoft@gmail.com>
2023-04-03 11:48:10 -0600

    fix: Add missing imports

--- src/app/shared/layouts/primary-layout/primary-layout.module.ts
+++ src/app/shared/layouts/primary-layout/primary-layout.module.ts
@@ -1,26 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { PrimaryLayoutComponent } from './primary-layout.component';
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+import { FooterModule } from '@shared/ui-components/footer/footer.module';
+import { IconModule } from '@shared/ui-components/icon/icon.module';
+import { LogoModule } from '@shared/ui-components/logo/logo.module';
+import { ToolbarModule } from '@shared/ui-components/toolbar/toolbar.module';
+import { PrimaryLayoutComponent } from './primary-layout.component';
 
 @NgModule({
   declarations: [PrimaryLayoutComponent],
   imports: [
     CommonModule,
     RouterModule,
-    MatToolbarModule,
     MatButtonModule,
-    MatIconModule,
     MatSidenavModule,
     MatListModule,
     MatDividerModule,
+    ToolbarModule,
+    LogoModule,
+    IconModule,
+    FooterModule,
   ],
   exports: [PrimaryLayoutComponent],
 })
