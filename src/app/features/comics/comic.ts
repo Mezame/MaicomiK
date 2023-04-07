274557cc3f14af2dc41d830f552cf9abb4ecaad6
Mezame <mezamesoft@gmail.com>
2023-04-07 13:20:22 -0600

    fix: Add null for some properties

--- src/app/features/comics/comic.ts
+++ src/app/features/comics/comic.ts
@@ -1,13 +1,13 @@
 import { Timestamp } from '@angular/fire/firestore';
 
 export interface Comic {
-  coverUrl?: string;
+  coverUrl?: string | null;
   title: string;
   format: ComicFormat;
   status: ComicStatus;
   chapter: number;
-  readers?: [{ name: string; url?: string }];
-  notes?: string;
+  readers?: [{ name: string; url?: string | null }] | null;
+  notes?: string | null;
   metadata: {
     id: string;
     urlSegment: string;
