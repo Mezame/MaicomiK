51deacd723a2dad3d881bff5da3752badf1e64e7
Mezame <mezamesoft@gmail.com>
2023-04-11 17:08:13 -0600

    feat: Add comic-notes-add-page functionality

--- src/app/home/comic-notes-add-page/comic-notes-add-page.component.ts
+++ src/app/home/comic-notes-add-page/comic-notes-add-page.component.ts
@@ -1,4 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Comic } from '@features/comics/comic';
+import { ComicsStoreService } from '@features/comics/comics-store.service';
+import { selectComic } from '@features/comics/state/comics.selectors';
+import { Store } from '@ngrx/store';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-comic-notes-add-page',
@@ -7,5 +13,96 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ComicNotesAddPageComponent {
+  comic$: Observable<Readonly<Comic>>;
+  comicUrlSegment: string;
+  comicReaders!: Partial<Comic['notes']>;
+  updatedComic!: Readonly<Comic>;
+  isSubmitButtonDisabled: boolean;
 
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store,
+    private comicsStoreService: ComicsStoreService,
+    private router: Router
+  ) {
+    this.comicUrlSegment = this.route.snapshot?.params['comicUrlSegment'];
+
+    this.comic$ = this.store.select(selectComic(this.comicUrlSegment)).pipe(
+      map((comic) => {
+        if (!comic) {
+          this.store.dispatch({ type: '[Comic Notes Add Page] Load Comics' });
+
+          return {} as unknown as Readonly<Comic>;
+        }
+
+        return comic;
+      })
+    );
+
+    this.isSubmitButtonDisabled = true;
+  }
+
+  getFormAction(event: {
+    action: string;
+    data: {
+      comicNotesFormValue: string | null;
+      isComicNotesFormValid: boolean;
+      originalComic: Readonly<Comic>;
+    };
+  }) {
+    let action: string;
+    let comicNotesFormValue: string | null;
+    let isComicNotesFormValid: boolean;
+    let comicNotes: Comic['notes'];
+    let originalComic: Readonly<Comic>;
+
+    action = event.action;
+    comicNotesFormValue = event.data.comicNotesFormValue;
+    isComicNotesFormValid = event.data.isComicNotesFormValid;
+    originalComic = { ...event.data.originalComic };
+
+    if (action == 'addComicNotes') {
+      if (isComicNotesFormValid) {
+        comicNotes = comicNotesFormValue as Comic['notes'];
+
+        this.updatedComic = { ...originalComic, notes: comicNotes };
+
+        this.isSubmitButtonDisabled = false;
+      } else {
+        this.isSubmitButtonDisabled = true;
+      }
+    }
+  }
+
+  addComicNotes() {
+    this.isSubmitButtonDisabled = true;
+
+    this.store.dispatch({
+      type: '[Comic Notes Add Page] Update Comic',
+      comic: this.updatedComic,
+    });
+
+    this.comicsStoreService.getApiState().subscribe((apiState) => {
+      if (
+        apiState?.operation == 'updateComic' &&
+        apiState.status == 'failure'
+      ) {
+        setTimeout(() => {
+          this.isSubmitButtonDisabled = false;
+        }, 3000);
+
+        return;
+      }
+
+      this.navigateToComicDetailPage();
+    });
+  }
+
+  cancel() {
+    this.navigateToComicDetailPage();
+  }
+
+  private navigateToComicDetailPage() {
+    this.router.navigate(['/home', 'comics', this.comicUrlSegment]);
+  }
 }
--- src/app/home/comic-notes-add-page/comic-notes-add-page.module.ts
+++ src/app/home/comic-notes-add-page/comic-notes-add-page.module.ts
@@ -1,13 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ComicNotesAddEditFormModule } from '@features/comics/comic-notes-add-edit-form/comic-notes-add-edit-form.module';
+import { FooterPortalModule } from '@shared/layouts/primary-layout/footer-portal/footer-portal.module';
 import { ComicNotesAddPageComponent } from './comic-notes-add-page.component';
 
 const routes: Routes = [{ path: '', component: ComicNotesAddPageComponent }];
 
 @NgModule({
   declarations: [ComicNotesAddPageComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ComicNotesAddEditFormModule,
+    MatButtonModule,
+    FooterPortalModule,
+  ],
 })
 export class ComicNotesAddPageModule {}
