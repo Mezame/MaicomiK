00d6602fee78e9f7fb5a367d7c968067d5e1f757
Mezame <mezamesoft@gmail.com>
2023-04-11 17:07:04 -0600

    feat: Add comic-notes-add-edit-form add notes functionality

--- src/app/features/comics/comic-notes-add-edit-form/comic-notes-add-edit-form.component.ts
+++ src/app/features/comics/comic-notes-add-edit-form/comic-notes-add-edit-form.component.ts
@@ -1,11 +1,70 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Comic } from '../comic';
 
 @Component({
   selector: 'app-comic-notes-add-edit-form',
   templateUrl: './comic-notes-add-edit-form.component.html',
   styleUrls: ['./comic-notes-add-edit-form.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ComicNotesAddEditFormComponent {
+  comicNotesForm!: FormControl<string | null>;
 
+  @Input('data') comic!: Readonly<Comic>;
+
+  @Input() action!: string;
+
+  @Output() actionEvent = new EventEmitter<{
+    action: string;
+    data: {
+      comicNotesFormValue: string | null;
+      isComicNotesFormValid: boolean;
+      originalComic: Readonly<Comic>;
+    };
+  }>();
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.comicNotesForm = this.fb.control('', Validators.required);
+  }
+
+  onValueChanges() {
+    let comicNotesFormValue: string | null;
+    let isComicNotesFormValid: boolean;
+
+    comicNotesFormValue = this.comicNotesForm.value;
+    isComicNotesFormValid = this.comicNotesForm.valid;
+
+    if (this.action == 'addComicNotes') {
+      if (!isComicNotesFormValid) {
+        this.emitAddComicNotes('', isComicNotesFormValid);
+
+        return;
+      }
+
+      this.emitAddComicNotes(comicNotesFormValue, isComicNotesFormValid);
+    }
+  }
+
+  emitAddComicNotes(
+    comicNotesFormValue: string | null,
+    isComicNotesFormValid = false
+  ) {
+    const action = 'addComicNotes';
+    const data = {
+      comicNotesFormValue,
+      isComicNotesFormValid,
+      originalComic: this.comic,
+    };
+
+    this.actionEvent.emit({ action, data });
+  }
 }
--- src/app/features/comics/comic-notes-add-edit-form/comic-notes-add-edit-form.module.ts
+++ src/app/features/comics/comic-notes-add-edit-form/comic-notes-add-edit-form.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComicNotesAddEditFormComponent } from './comic-notes-add-edit-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [ComicNotesAddEditFormComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   exports: [ComicNotesAddEditFormComponent],
 })
 export class ComicNotesAddEditFormModule {}
