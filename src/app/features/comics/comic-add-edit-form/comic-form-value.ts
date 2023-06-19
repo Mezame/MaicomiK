2eac017750b7e47c3a70673ae844fa5bfe33830b
Mezame <mezamesoft@gmail.com>
2023-06-18 20:09:47 -0600

    feat: Add ComicForm type

--- src/app/features/comics/comic-add-edit-form/comic-form-value.ts
+++ src/app/features/comics/comic-add-edit-form/comic-form-value.ts
@@ -1,9 +1,18 @@
+import { FormControl, FormGroup } from '@angular/forms';
 import { ComicFormat, ComicStatus } from '../comic';
+
+export type ComicForm = FormGroup<{
+  title: FormControl<ComicFormValue['title']>;
+  format: FormControl<ComicFormValue['format']>;
+  status: FormControl<ComicFormValue['status']>;
+  chapter: FormControl<ComicFormValue['chapter']>;
+  coverUrl: FormControl<ComicFormValue['coverUrl']>;
+}>;
 
 export interface ComicFormValue {
   title: string | null;
-  format: ComicFormat | string | null;
-  status: ComicStatus | string | null;
+  format: ComicFormat | null;
+  status: ComicStatus | null;
   chapter: string | null;
   coverUrl: string | null;
 }
