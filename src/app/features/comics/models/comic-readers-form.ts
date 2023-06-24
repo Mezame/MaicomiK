ede80e6069dfb5bff732b2acf6734b1b84fdd869
Mezame <mezamesoft@gmail.com>
2023-06-24 08:25:10 -0600

    style: Change interface order

--- src/app/features/comics/models/comic-readers-form.ts
+++ src/app/features/comics/models/comic-readers-form.ts
@@ -1,13 +1,13 @@
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
+export interface ComicReadersFormValue {
+  name: string | null;
+  url: string | null;
+}
+
 export type ComicReadersForm = FormGroup<{
   name: FormControl<ComicReadersFormValue['name']>;
   url: FormControl<ComicReadersFormValue['url']>;
 }>;
 
 export type ComicReadersFormArray = FormArray<ComicReadersForm>;
-
-export interface ComicReadersFormValue {
-  name: string | null;
-  url: string | null;
-}
