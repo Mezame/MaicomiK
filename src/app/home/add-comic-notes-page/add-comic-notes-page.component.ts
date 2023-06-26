347afc807b8e559822880a2ac2b64a27758935e7
Mezame <mezamesoft@gmail.com>
2023-06-25 20:56:38 -0600

    fix: Refactor code

--- src/app/home/add-comic-notes-page/add-comic-notes-page.component.ts
+++ src/app/home/add-comic-notes-page/add-comic-notes-page.component.ts
@@ -1,8 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Comic, ComicNotesFormValue } from '@features/comics/models';
-import { selectComic } from '@features/comics/state';
-import { Observable, map } from 'rxjs';
+import {
+  AddEditComicNotesEvent,
+  Comic,
+  ComicNotesFormValue,
+  EventBus,
+} from '@features/comics/models';
+import { Observable } from 'rxjs';
 import { AddComicNotesFacadeService } from './add-comic-notes-facade.service';
 
 @Component({
@@ -11,59 +20,27 @@ import { AddComicNotesFacadeService } from './add-comic-notes-facade.service';
   styleUrls: ['./add-comic-notes-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddComicNotesPageComponent {
-  comic$: Observable<Readonly<Comic>>;
-  comicUrlSegment: string;
+export class AddComicNotesPageComponent implements OnInit, OnDestroy {
+  comic$!: Observable<Readonly<Comic>>;
+  comicUrlSegment!: string;
   updatedComic!: Comic;
-  isSubmitButtonDisabled: boolean;
+  isSubmitButtonDisabled!: boolean;
 
   constructor(
     private addComicNotesFacadeService: AddComicNotesFacadeService,
     private route: ActivatedRoute,
     private router: Router
-  ) {
-    this.comicUrlSegment = this.route.snapshot?.params['comicUrlSegment'];
+  ) {}
 
-    this.comic$ = this.addComicNotesFacadeService.getComic(
-      this.comicUrlSegment
-    );
-
-    this.isSubmitButtonDisabled = true;
+  ngOnInit(): void {
+    this.setInitialValues();
   }
 
-  getFormAction(event: {
-    action: string;
-    data: {
-      comicNotesFormValue: ComicNotesFormValue;
-      isComicNotesFormValid: boolean;
-      originalComic: Readonly<Comic>;
-    };
-  }) {
-    let action: string;
-    let comicNotesFormValue: ComicNotesFormValue;
-    let isComicNotesFormValid: boolean;
-    let comicNotes: Comic['notes'];
-    let originalComic: Readonly<Comic>;
-
-    action = event.action;
-    comicNotesFormValue = event.data.comicNotesFormValue;
-    isComicNotesFormValid = event.data.isComicNotesFormValid;
-    originalComic = { ...event.data.originalComic };
-
-    if (action == 'addComicNotes') {
-      if (isComicNotesFormValid) {
-        comicNotes = comicNotesFormValue as Comic['notes'];
-
-        this.updatedComic = { ...originalComic, notes: comicNotes };
-
-        this.isSubmitButtonDisabled = false;
-      } else {
-        this.isSubmitButtonDisabled = true;
-      }
-    }
+  ngOnDestroy(): void {
+    this.addComicNotesFacadeService.clearApiState();
   }
 
-  addComicNotes() {
+  addComicNotes(): void {
     this.isSubmitButtonDisabled = true;
 
     this.addComicNotesFacadeService.addComicNotes(this.updatedComic);
@@ -84,13 +61,52 @@ export class AddComicNotesPageComponent {
     });
   }
 
-  cancel() {
+  cancelAddComicNotes(): void {
     this.navigateToComicDetailPage();
   }
 
-  private navigateToComicDetailPage() {
+  onEventBus(event: EventBus): void {
+    let eventName: string;
+
+    eventName = event.name;
+
+    if (eventName == 'addComicNotes') {
+      this.prepareToAddComicNotes(event);
+    }
+  }
+
+  prepareToAddComicNotes(event: AddEditComicNotesEvent): void {
+    let comicNotesFormValue: ComicNotesFormValue;
+    let isComicNotesFormValid: boolean;
+    let comicNotes: Comic['notes'];
+    let originalComic: Readonly<Comic>;
+
+    comicNotesFormValue = event.data.comicNotesFormValue;
+    isComicNotesFormValid = event.data.isComicNotesFormValid;
+    originalComic = { ...event.data.originalComic };
+
+    if (isComicNotesFormValid) {
+      comicNotes = comicNotesFormValue as Comic['notes'];
+
+      this.updatedComic = { ...originalComic, notes: comicNotes };
+
+      this.isSubmitButtonDisabled = false;
+    }
+  }
+
+  private navigateToComicDetailPage(): void {
     this.router
       .navigate(['/home', 'comics', this.comicUrlSegment])
       .catch((error) => error);
+  }
+
+  private setInitialValues(): void {
+    this.comicUrlSegment = this.route.snapshot?.params['comicUrlSegment'];
+
+    this.comic$ = this.addComicNotesFacadeService.getComic(
+      this.comicUrlSegment
+    );
+
+    this.isSubmitButtonDisabled = true;
   }
 }
