f77dfeb7baf52014671a296b5eb9c1a8e0398227
Mezame <mezamesoft@gmail.com>
2023-07-06 09:15:00 -0600

    feat: Add comic-detail-services interface

---
+++ src/app/home/comic-detail-page/comic-detail.ts
@@ -0,0 +1,16 @@
+import { ApiState } from '@core/services/app-store.service';
+import { Comic } from '@features/comics/models';
+import { Observable } from 'rxjs';
+
+export interface ComicDetailServices {
+  clearApiState(): void;
+  deleteComic(comicId: string): void;
+  deleteComicNotes(comic: Readonly<Comic>, comicFields: Partial<Comic>): void;
+  deleteComicReaders(comic: Readonly<Comic>, comicFields: Partial<Comic>): void;
+  getApiState(): Observable<ApiState>;
+  incrementComicChapter(
+    comic: Readonly<Comic>,
+    comicFields: Partial<Comic>
+  ): void;
+  loadComics(): void;
+}
