7a73961b1406e0aee3a5c3dfbc87cb51fb8d4783
Mezame <mezamesoft@gmail.com>
2023-07-05 22:08:52 -0600

    feat: Add add-comic-notes-page-services interface

---
+++ src/app/home/add-comic-notes-page/add-comic-notes-page.ts
@@ -0,0 +1,11 @@
+import { ApiState } from '@core/services/app-store.service';
+import { Comic } from '@features/comics/models';
+import { Observable } from 'rxjs';
+
+export interface AddComicNotesPageServices {
+  addComicNotes(comic: Comic): void;
+  clearApiState(): void;
+  getApiState(): Observable<ApiState>;
+  getComic(comicUrlSegment: string): Observable<Readonly<Comic>>;
+  loadComics(): void;
+}
