f58404c3bb08c8b7c1215070d2812a8e677e24b5
Mezame <mezamesoft@gmail.com>
2023-07-08 17:10:37 -0600

    fix: Remove unused change-detection

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
   template: '<router-outlet></router-outlet>',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
