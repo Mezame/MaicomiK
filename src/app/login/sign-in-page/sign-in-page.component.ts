ba2bc801008cf2499af24ed0742f6ebf465c78f8
Mezame <mezamesoft@gmail.com>
2023-07-08 17:10:01 -0600

    feat: Add sign-in component

---
+++ src/app/login/sign-in-page/sign-in-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  templateUrl: './sign-in-page.component.html',
+  styleUrls: ['./sign-in-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SignInPageComponent {
+
+}
---
+++ src/app/login/sign-in-page/sign-in-page.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignInPageComponent } from './sign-in-page.component';
+
+@NgModule({
+  declarations: [SignInPageComponent],
+  imports: [CommonModule],
+})
+export class SignInPageModule {}
