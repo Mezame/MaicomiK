624878870905c835c97b2b0774f3d4167ae62625
Mezame <mezamesoft@gmail.com>
2023-07-08 17:36:57 -0600

    style: Sort code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
-  path: 'welcome',
-  loadChildren: () =>
-    import('./welcome/welcome.module').then((m) => m.WelcomeModule)
-},
-{
-  path: 'home',
-  loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
-},
-{
-  path: '',
-  redirectTo: 'home',
-  pathMatch: 'full',
-},
-//{ path: '**', redirectTo: 'login' }
+const routes: Routes = [
+  {
+    path: 'welcome',
+    loadChildren: () =>
+      import('./welcome/welcome.module').then((m) => m.WelcomeModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  //{ path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
